9c39bfcd1579cdf5c5df0cf572dcb46ab84d2795
Ifeoma <ojiniifeoma@gmail.com>
2021-08-25 04:04:37 -0700

    Fixed betty error

--- shell.h
+++ shell.h
@@ -19,7 +19,7 @@ unsigned int nbr_spaces(char *s);
 char **stringToTokens(char *str);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
-char* _strtok(char* s, char d);
+char *_strtok(char *s, char d);
 char *_strdup(char *str);
 char *_getenv(char *name, char **_environ);
 
